a234fce86c9f90c79f4dfa8dde773771c1495d60
Alejandro Linan <jandrolinan@gmail.com>
2020-09-16 13:22:54 -0500

    added some arrays to test code with

--- jumping-on-the-clouds/solution.js
+++ jumping-on-the-clouds/solution.js
@@ -2,7 +2,7 @@ function jumpingOnClouds(c) {
     let jumps = 0
 
     for (let i = 0; i < c.length; ) {
-    // console.log(i)
+    console.log(i)
     if((c[i] === 0) && (c[(i + 1)] === 0) && (c[(i + 2)] === 1)){
         jumps += 1
         i += 1
@@ -19,3 +19,11 @@ function jumpingOnClouds(c) {
 
     return jumps
 }
+
+// c = [0, 0, 0, 1, 0, 0] // 3
+// c = [0, 0, 1, 0, 0, 1, 0] // 4
+// c = [0, 0, 1, 0, 0, 0, 0, 1, 0, 0] // 6
+c = [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0] // 28
+
+
+console.log(jumpingOnClouds(c))
